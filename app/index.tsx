9fda4feafabaa4c85807a2f689628c66ca9acbb3
ekduszla <dayeonkim0228@gmail.com>
2025-10-17 02:18:12 +0900

    페이지 생성

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>희희희</Text>
     </View>
   );
 }
